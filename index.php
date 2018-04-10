01e0050cde2db7c1574c533c795c3d30eacb9be8
stsegaye2 <stsegaye2@student.gsu.edu>
2018-04-10 18:59:11 -0400

    add files via upload

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+	<html>
+		<head>
+			<title> Raceway </title>
+			<link rel="stylesheet" type="text/css" href="try.css">
+		</head>
+		<body>
+			<p>Raceway productive sheet</p>
+			<div>
+				<button class="firstButtons"> day layout</button>
+				<br>
+				<button class="firstButtons">view day progress</button>
+			</div>
+			<div id ="tasks">
+				<ul>
+					<li class="task">Task 1</li>
+					<li class="task">Task 2</li>
+					<li class="task">Task 3</li>
+					<li class="task">Task 4</li>
+					<li class="task">Task 5</li>
+					<li class="task">Task 6</li>
+					<li class="task">Task 7</li>
+					<li class="task">Task 8</li>
+					<li class="task">Task 9</li>
+					<li class="task">Task 10</li>
+				</ul>
+			</div>
+			<button>end day</button>
+		</body>
+	</html>
